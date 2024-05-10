9e55e521785f57f473bd8465614fcb741c1b6990
ErikBergloef <47384098+ErikBergloef@users.noreply.github.com>
2024-05-10 08:56:08 +0200

    Add files via upload

---
+++ Stuck In A Time Loop/Stuck In A Time Loop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Stuck
+{
+    class Abracadabra
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What was the number the wizard gave you?");
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < magicNumber + 1; i++) 
+            { 
+            Console.WriteLine(i + " " + "Abracadabra");
+            }
+
+        }
+    }
+}
